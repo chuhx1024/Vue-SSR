721b105f0e9866c443334a26930d25a3cc59519d
chuhx <chuhx1024@126.com>
2021-09-25 10:15:28 +0800

    feat: Add eslint .vscode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-    <!-- 客户端渲染的入口节点 --> 
-    <div id="app"> 
-            <h1>Vue SSR</h1> 
-    </div> 
-</template> 
-<script> 
-export default { 
-    name: 'App' }
-</script> 
+    <!-- 客户端渲染的入口节点 -->
+    <div id="app">
+        <h1>Vue SSR</h1>
+    </div>
+</template>
+<script>
+export default { name: 'App' }
+</script>
 <style> </style>
