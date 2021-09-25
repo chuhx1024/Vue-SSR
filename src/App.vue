509cdddf35d5f8d5fcafa7b767138a036ca67083
chuhx <chuhx1024@126.com>
2021-09-25 09:53:25 +0800

    feat: entry

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- 客户端渲染的入口节点 --> 
+    <div id="app"> 
+            <h1>Vue SSR</h1> 
+    </div> 
+</template> 
+<script> 
+export default { 
+    name: 'App' }
+</script> 
+<style> </style>
